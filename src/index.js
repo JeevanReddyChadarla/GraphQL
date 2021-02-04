aa8f7d9037452606f879367871bc29305e16dbe3
jeevan <jeevanchadarla1998@gmail.com>
2021-02-04 14:36:02 +0530

    Arrays used

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import {GraphQLServer} from 'graphql-yoga';
 
 const typeDefs = `
 type Query{
+    greeting(name: String): String!
+    add(numbers: [Int!]!): Int!
+    grades : [Int!]!
     me: User!
     post : Post!
 }
@@ -24,7 +27,28 @@ type Post{
 `
 const resolvers={
     Query: {
-        
+
+        greeting(parent,args,ctx,info)
+        {
+            if(args.name){
+                return `Hello ${args.name}`
+            }
+            else{
+                return `hello`
+            }
+        },
+        add(parent, args, ctx, info)
+        {  
+                if(args.numbers.length === 0){
+                    return 0
+                }
+                return args.numbers.reduce((accumulator, currentValue) => {
+                    return accumulator+currentValue
+                })
+        },
+        grades(parent,args,ctx,info){
+            return [98,35,63]
+        },
         me(){
             return{
                 id: "jee112",
